ec1cda5f40e8daffb2701814c13f0942822b1f8b
Livia <liviamarresende@gmail.com>
2020-03-13 14:10:47 -0300

    adicionando index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<form action="script.php" method="post">
+nome: <input type=text name=nome><br>
+nome: <input type=text name=nome><br>
+datadenascimento: <input type=text name=datadenascimento><br>
+<input type=submit value="OK">
+</form>
