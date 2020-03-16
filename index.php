f51e448bf26ece16ac81eebd82149446cfb8e7cd
Livia <liviamarresende@gmail.com>
2020-03-16 14:28:00 -0300

    formulário de matrícula

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
+<html>
+<body>
+
+<hl>Matrícula</hl>
 <form action="script.php" method="post">
-nome: <input type=text name=nome><br>
-nome: <input type=text name=nome><br>
-datadenascimento: <input type=text name=datadenascimento><br>
+Nome: <input type=text name=Nome><br>
+Data de nascimento: <input id="date" type="date"><br>
+Escolha do curso: <form action="select.php" method="post">
+<select name=processador>
+<option value=Manucire>Manicure</option>
+<option value=Informática>Informática</option>
+<option value=Mecanica>Mecanica</option>
+</select><BR><BR>
 <input type=submit value="OK">
 </form>
+<form>
