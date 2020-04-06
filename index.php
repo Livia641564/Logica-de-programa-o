339380b350671134070ea1398164793ddfac61e3
Livia641564 <liviamarresende@gmail.com>
2020-04-06 12:10:34 -0300

    Formulario]

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
 <html>
+    <head>
+        <title> Formulário </title>
+        <style type="text/css">
+                body{background-color:#eeeeee}
+                hl{font-weight:bold;}
+                .bt{cursor:pointer;}
+        </style>
+    </head>
     <body>
-
-    <hl>Matrícula</hl>
-    <form action="script.php" method="post">
-    Nome: <input type=text name=Nome><br>
-    Data de nascimento: <input id="date" type="date"><br>
-    Escolha do curso: <form action="select.php" method="post">
-        <select name=processador>
-        <option value=Manucire>Manicure</option>
-        <option value=Informática>Informática</option>
-        <option value=Mecanica>Mecanica</option>
+        <hl>Matrícula</hl>
+        <form action="script.php" method="post">
+        Nome: <input type="nome" name="nome"><br>
+        Data de nascimento: <input id="date" type="date"><br>
+        Escolha do curso: <form action="select.php" method="post">
+            <select name=processador>
+            <option value=Manucire>Manicure</option>
+            <option value=Informática>Informática</option>
+            <option value=Mecanica>Mecanica</option>
         </select><BR><BR>
-    <input type=submit value="OK">
-</form>
+        <input type=submit class="bts" value="OK">
+        </form>
+</html>
